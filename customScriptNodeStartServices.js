493744d27b3b4bf21654a71002eca1d3c19cf9c3
Presa <ivopresa@gmail.com>
2023-11-28 19:49:11 +0100

    add OS identify and open browser with frontend link

--- customScriptNodeStartServices.js
+++ customScriptNodeStartServices.js
@@ -1,5 +1,30 @@
-import { exec } from 'child_process';
+import {exec} from 'child_process';
 import axios from 'axios';
+
+const checkBackendHealth = async () => {
+  try {
+    await axios.get('http://localhost:3003/ping');
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+const openBrowser = (url) => {
+  switch (process.platform) {
+    case 'darwin':
+      exec(`open ${url}`);
+      console.log(`Identified macOS opening ${url}`)
+      break;
+    case 'win32':
+      exec(`start ${url}`);
+      console.log(`Identified Windows opening ${url}`)
+      break;
+    default:
+      exec(`xdg-open ${url}`);
+      console.log(`Identified Linux opening ${url}`)
+  }
+};
 
 const startBackend = () => {
   return new Promise((resolve, reject) => {
@@ -19,23 +44,16 @@ const startBackend = () => {
   });
 };
 
-const checkBackendHealth = async () => {
-  try {
-    await axios.get('http://localhost:3003/ping');
-    return true;
-  } catch (error) {
-    return false;
-  }
-};
-
 const startFrontend = () => {
   return new Promise((resolve, reject) => {
+    let localUrl;
     const frontend = exec('npm install && npm run dev');
 
     frontend.stdout.on('data', (data) => {
       console.log(`Frontend: ${data}`);
       if (data.includes('Local:')) {
-        resolve();
+        localUrl = data.match(/http:\/\/[^\s]+/)[0]; // Extract URL from the console output
+        resolve(localUrl);
       }
     });
 
@@ -58,8 +76,12 @@ const init = async () => {
         await new Promise(resolve => setTimeout(resolve, 2000));
       }
     }
-    await startFrontend();
+    const localUrl = await startFrontend();
     console.log('Frontend started successfully.');
+
+    if (localUrl) {
+      openBrowser(localUrl);
+    }
   } catch (error) {
     console.error('Failed to start services:', error);
   }
