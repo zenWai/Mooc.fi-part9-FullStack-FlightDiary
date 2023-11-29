66197e2bc690238df4b20605505a4f3ed3c33de0
Presa <ivopresa@gmail.com>
2023-11-29 10:15:38 +0100

    start() and stop() server do not run when imported by tests

--- flightdiary-backend/src/index.ts
+++ flightdiary-backend/src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import diaryRouter from './routes/diaries';
 import cors from 'cors';
+import { Server } from 'http';
 
-const app = express();
+export const app = express();
 app.use(cors());
 app.use(express.json());
 
 const PORT = 3003;
-
+let server: Server;
 
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
@@ -16,6 +17,19 @@ app.get('/ping', (_req, res) => {
 
 app.use('/api/diaries', diaryRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export const start = () => {
+  if (!server?.listening) {
+    server = app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  }
+};
+
+export const stop = () => {
+  server.close();
+};
+
+// Only start the server if the file is run directly (not when imported for tests)
+if (require.main === module) {
+  start();
+}
