20db249bb25f9bc5a3647745808a1abbb15549e3
Presa <ivopresa@gmail.com>
2023-11-29 15:11:56 +0100

    Vite plugins. viteCommonjs(), visualizer(), VitePWA()

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteCommonjs(),
+    visualizer(),
+    VitePWA(),
+  ],
+});
